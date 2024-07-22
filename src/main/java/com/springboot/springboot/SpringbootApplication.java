f4cad8b6136cef61959fc118de1cd1cde15e10b0
VIVEK YADAV <yadav7738vivek@gmail.com>
2024-07-22 05:54:55 +0530

    first

---
+++ src/main/java/com/springboot/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		// System.out.println("hello world");
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
